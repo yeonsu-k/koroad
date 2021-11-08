d8228ffaf956bde749f048991dbdea3ff4f0fda6
yeonsu-k <83412032+yeonsu-k@users.noreply.github.com>
2021-11-08 17:41:14 +0900

    Add files via upload

---
+++ in.php
@@ -0,0 +1,10 @@
+<html>
+  <body>
+    <div id=”r-output” id=”width: 100%; padding: 25px;”>
+      <?php
+        exec("sample.R");
+      ?>
+    <img src="test.png" alt="R Graph" />
+    </div>
+  </body>
+</html>
