0504c92601c350a607a7c729bb80d7a81c4bf7bb
yeonsu-k <83412032+yeonsu-k@users.noreply.github.com>
2021-11-09 12:45:38 +0900

    Add files via upload

--- in.php
+++ in.php
@@ -2,9 +2,10 @@
   <body>
     <div id=”r-output” id=”width: 100%; padding: 25px;”>
       <?php
-        exec("sample.R");
+        exec("Rscript sample.R");
       ?>
-    <img src="test.png" alt="R Graph" />
+      <img src="../img/test.jpg" alt="R Graph" />
     </div>
   </body>
 </html>
+
--- sample.R
+++ sample.R
@@ -1,4 +1,4 @@
 x <- rnorm(6,0,1)
-png(filename="test.png", width=500, height=500)
+jpeg(filename="test.jpg", width=500, height=500)
 hist(x, col="red")
 dev.off()
