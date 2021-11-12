59a5e2808ca3671156deb070488dd984cb680d89
yeonsu-k <83412032+yeonsu-k@users.noreply.github.com>
2021-11-12 09:41:42 +0900

    Update in.php

--- in.php
+++ in.php
@@ -2,7 +2,7 @@
   <body>
     <div id=”r-output” id=”width: 100%; padding: 25px;”>
       <?php
-        exec("Rscript sample.R");
+        exec("sample.R");
       ?>
       <img src="../img/test.jpg" alt="R Graph" />
     </div>
