32c49ce785a3e79f1891f635adebbb10f82da0cf
yeonsu-k <dustn4325@naver.com>
2021-11-30 16:35:10 +0900

    2021.11.30

--- filezila/sample.R
+++ filezila/sample.R
@@ -1,4 +1,15 @@
+# x <- rnorm(6,0,1)
+# jpeg(filename="test.jpg", width=500, height=500)
+# hist(x, col="yellow")
+# dev.off()
+
+
+# my_rscript.R<font></font>
+
 x <- rnorm(6,0,1)
-jpeg(filename="test.jpg", width=500, height=500)
-hist(x, col="red")
+png(filename="temp.jpg", width=500, height=500)
+hist(x, col="lightblue")
 dev.off()
+
+# my_rscript.R
+
