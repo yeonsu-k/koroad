a8a24c1c042f866b338579b100671467c6a9bdce
yeonsu-k <83412032+yeonsu-k@users.noreply.github.com>
2021-12-13 17:31:46 +0900

    Add files via upload

---
+++ left.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+ <div align="middle">
+<?php
+ #! /bin/bash
+ exec("Rscript sample.R");
+ echo "<img src='test.png'/>";
+?>
+</div>
+</body>
+</html>
+
+
--- sample.R
+++ sample.R
@@ -1,4 +1,6 @@
-x <- rnorm(6,0,1)
-jpeg(filename="test.jpg", width=500, height=500)
-hist(x, col="red")
+#! /usr/bin/env Rscript
+set.seed(1)
+data=rnorm(100,170,5)
+png(filename="test.png",width=500,height=500)
+hist(data)
 dev.off()
